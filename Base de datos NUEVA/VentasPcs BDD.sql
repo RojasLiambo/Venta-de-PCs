2ffc56fdc3c3c9d57e99e977de03f5b64507228b
MicheliLucio <91755189+MicheliLucio@users.noreply.github.com>
2022-11-28 04:06:27 -0300

    Se actualizo la base de datos hecha por Micheli

--- Base de datos NUEVA/VentasPcs BDD.sql
+++ Base de datos NUEVA/VentasPcs BDD.sql
@@ -1,47 +1,28 @@
+drop database if exists VentasPcs;
+create database VentasPcs;
+use VentasPcs;
 create table cliente(
 	nombre varchar(50),
-	dni integer,
-	correo varchar(100),
-	telefono varchar(12),
+	dni integer not null,
 	correo varchar(100),
 	telefono varchar(12),
 	primary key (dni)
 );
 create table empleado(
 	nombre varchar(50),
-	legajo integer,
-	horario time,
-	telefono varchar(12),
+	legajo integer not null,
+	horario time not null,
 	primary key (legajo)
 );
-create table facturacion(
-	nFactura integer,
-	dni integer,
-	legajo integer,
-	fechafactu date,
-	metodocompra varchar(50),
-	primary key (nFactura),
-	foreign key (dni) references cliente (dni),
-	foreign key (legajo) references empleado(legajo)
-);
+
 
 create table computadora(
-	numeroComputadora integer,
-	esArmada boolean,
+	numeroComputadora integer not null auto_increment,
 	precio double,
 	paisorigen varchar(50),
 	primary key (numeroComputadora)
 );
 
-create table linea(
-	nLinea integer,
-	nFactura integer,
-	numeroComputadora integer,
-	cant integer,
-	primary key (nLinea),
-	foreign key(nFactura) references facturacion (nFactura),
-	foreign key(numeroComputadora) references computadora(numeroComputadora)
-);
 
 create table componentes(
 	numeroSerie integer,
@@ -50,15 +31,38 @@ create table componentes(
 	precio double,
 	impuesto double,
 	descripcion varchar(500),
-	primary key (numeroComputadora)
+	primary key (numeroSerie)
+);
+
+create table facturacion(
+	nFactura integer not null auto_increment,
+	numeroComputadora integer,
+	dni integer,
+	legajo integer,
+	fechafactu date,
+	metodocompra varchar(50),
+	primary key (nFactura),
+	foreign key (dni) references cliente (dni),
+	foreign key (numeroComputadora) references computadora (numeroComputadora),
+	foreign key (legajo) references empleado(legajo)
 );
 
 create table compc(
 	numeroComputadora integer,
-	codigoComponente integer,
+	numeroSerie integer,
 	foreign key (numeroComputadora) references computadora (numeroComputadora),
 	foreign key (numeroSerie) references componentes (numeroSerie)
 );
+create table linea(
+	nLinea integer not null auto_increment,
+	nFactura integer,
+	numeroComputadora integer,
+	cant integer,
+	primary key (nLinea),
+	foreign key(nFactura) references facturacion (nFactura),
+	foreign key(numeroComputadora) references computadora(numeroComputadora)
+);
+
 
 insert into componentes values (01,"Procesador","Intel co.",100000.00,500.00,"3,65 ghz 10 nucleos");
 insert into componentes values (02,"Procesador","Intel co.",9000.00,500.00,"3,00 Ghz 2 nucleos");
@@ -74,11 +78,11 @@ insert into componentes values (11,"TarjetaGrafica","AMD Inc.",299999.00,2500.00
 insert into componentes values (12,"TarjetaGrafica","Nvidia Co.",3.00,0.01,"MTX 4050 con 244 gb de memoria");
 insert into componentes values (13,"Fuente","Evga",6567.00, 500.00,"Fuente de 750w reales");
 insert into componentes values (14,"Fuente","Evga",678932.00,7000.00,"Fuente de 5000w reales");
-insert into componentes values (15,"Fuente","SUBLIME 4",4.00,,"4w reales");
+insert into componentes values (15,"Fuente","SUBLIME 4",4.00,0.04,"4w reales");
 insert into componentes values (16,"Fuente","Evga",0.32, 0.01,"1545323w reale");
 insert into componentes values (17,"Telcado","Redragon",13999.99,800.00,"semimecanico");
 insert into componentes values (18,"Teclado","Logitech",5165.00,500.00,"membrana");
-insert into componentes values (19,"Teclado","Apple",18000.00, 800.00,"Es un teclado gracioso de color blanco no es gamer pero es de membrana y se la re banca");
+insert into componentes values (19,"Teclado","Apple",18000.00, 800.00,"Es un teclado gracioso de color blanco de membrana");
 insert into componentes values (20,"Teclado","RAZER",0.69,0.01,"Gamer Mecanico");
 insert into componentes values (21,"Mouse","Logitech",8000.00,500.00,"mouse G305 Logitech con 12000 dpi");
 insert into componentes values (22,"Mouse","Razer",8999.00,500.00,"mouse Viper de Razer con 8500 dpi");
@@ -91,7 +95,50 @@ insert into componentes values (28,"Monitor","LG",5.00,0.01,"LG 20MKRSXR3 de 20Â
 insert into componentes values (29,"Mother","Msi",30500.00,800.00,"Mother MSI  B250m");
 insert into componentes values (30,"Mother","Asus",18650.00,800.00,"asus PRIME B460M-A");
 insert into componentes values (31,"Mother","Gigabyte",500.00,250.00,"Mother gb-ask Gygabyte");
-insert into componentes values (32,"Mother","MSI",0.27,,"UNA MOTHER 8250 M TERRIBLE MOTHER APROBADA POR SanTiAgo doRRegO de TNTECNO");
+insert into componentes values (32,"Mother","MSI",0.27,0.01,"UNA MOTHER 8250 M TERRIBLE MOTHER APROBADA POR SanTiAgo doRRegO de TNTECNO");
+
+insert into computadora(numeroComputadora, paisorigen) VALUES (01, "China");
+insert into computadora(numeroComputadora, paisorigen) VALUES (02, "Alemania");
+insert into computadora(numeroComputadora, paisorigen) VALUES (03, "Rusia");
+insert into computadora(numeroComputadora, paisorigen) VALUES (04, "Argentina");
+
+insert into compc values (01,01);
+insert into compc values (02,02);
+insert into compc values (03,03);
+insert into compc values (04,04);
+insert into compc values (01,05);
+insert into compc values (02,06);
+insert into compc values (03,07);
+insert into compc values (04,08);
+insert into compc values (01,09);
+insert into compc values (02,10);;
+insert into compc values (03,11);
+insert into compc values (04,12);
+insert into compc values (01,13);
+insert into compc values (02,14);
+insert into compc values (03,15);
+insert into compc values (04,16);
+insert into compc values (01,17);
+insert into compc values (02,18);
+insert into compc values (03,19);
+insert into compc values (04,20);
+insert into compc values (01,21);
+insert into compc values (02,22);
+insert into compc values (03,23);
+insert into compc values (04,24);
+insert into compc values (01,25);
+insert into compc values (02,26);
+insert into compc values (03,27);
+insert into compc values (04,28);
+insert into compc values (01,29);
+insert into compc values (02,30);
+insert into compc values (03,31);
+insert into compc values (04,32);
+
+insert into empleado values("Marmol Sofia",01,"8:00");
+insert into empleado values("Micheli Lucio",02,"12:00");
+insert into empleado values("Aguilera Nayla",03,"16:00");
+insert into empleado values("Rojas Javier",04,"20:00");
 
 
 
